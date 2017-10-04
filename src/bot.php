f47f7b517bb863852d4da7844ca8f6e102fee5a3
Marc Pignat <marc@pignat.org>
2017-10-04 16:05:20 +0200

    detect new server

--- src/bot.php
+++ src/bot.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 
 // Load composer
@@ -11,32 +10,55 @@ function online($uid, $name)
     $pdo = get_db();
     
     $sql = "INSERT INTO `ob_online` (`uid`, `time`) VALUES (:uid, :time)";
-        
-    //Prepare our statement.
     $statement = $pdo->prepare($sql);
-    
-    
-    //Bind our values to our parameters (we called them :make and :model).
     $statement->bindValue(':uid', $uid);
     $statement->bindValue(':time', time());
     
-    
-    //Execute the statement and insert our values.
+    // Execute the statement and insert our values.
     $inserted = $statement->execute();
     
-    //Because PDOStatement::execute returns a TRUE or FALSE value,
-    //we can easily check to see if our insert was successful.
-    if (!$inserted){
+    // Because PDOStatement::execute returns a TRUE or FALSE value,
+    // we can easily check to see if our insert was successful.
+    if (! $inserted) {
         return $inserted;
     }
+    
+    $sql = "SELECT * FROM `ob_online` where uid=:uid order by id DESC limit 2";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':uid', $uid);
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    $telegram = telegram();
+    
+    if ($statement->rowCount() == 1) {
+        
+        Longman\TelegramBot\Request::sendToActiveChats('sendMessage', // Callback function to execute (see Request.php methods)
+        [
+            'text' => "found a new server : $uid"
+        ], // Param to evaluate the request
+        [
+            'groups' => true,
+            'supergroups' => true,
+            'channels' => false,
+            'users' => true
+        ]);
+    }
+}
+
+function udpate_db()
+{
+    
 }
 
 function get_db()
 {
     global $mysql_credentials;
     
-    $dsn     = 'mysql:host=' . $mysql_credentials['host'] . ';dbname=' . $mysql_credentials['database'];
-    $options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . 'utf8mb4'];
+    $dsn = 'mysql:host=' . $mysql_credentials['host'] . ';dbname=' . $mysql_credentials['database'];
+    $options = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . 'utf8mb4'
+    ];
     $pdo = new PDO($dsn, $mysql_credentials['user'], $mysql_credentials['password'], $options);
     
     return $pdo;
@@ -44,37 +66,46 @@ function get_db()
 
 $pdo;
 
-function bot($is_hook)
+function telegram()
 {
     global $bot_api_key;
     global $bot_username;
+    
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+    
+    $commands_paths = [
+        __DIR__ . '/Commands'
+    ];
+    $telegram->addCommandsPaths($commands_paths);
+
+    // Enable MySQL
+    $pdo = get_db();
+    $telegram->enableExternalMySql($pdo);
+
+    // Longman\TelegramBot\TelegramLog::initErrorLog(__DIR__ . "/{$bot_username}_error.log");
+    // Longman\TelegramBot\TelegramLog::initDebugLog(__DIR__ . "/{$bot_username}_debug.log");
+    // Longman\TelegramBot\TelegramLog::initUpdateLog(__DIR__ . "/{$bot_username}_update.log");
+    return $telegram;
+}
+
+function bot($is_hook)
+{
     global $pdo;
     
     try {
         // Create Telegram API object
-        $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+        $telegram = telegram();
         
         $telegram->enableAdmin("28932656");
         
-        $commands_paths = [
-            __DIR__ . '/Commands'
-        ];
-        $telegram->addCommandsPaths($commands_paths);
-        //Longman\TelegramBot\TelegramLog::initErrorLog(__DIR__ . "/{$bot_username}_error.log");
-        //Longman\TelegramBot\TelegramLog::initDebugLog(__DIR__ . "/{$bot_username}_debug.log");
-        //Longman\TelegramBot\TelegramLog::initUpdateLog(__DIR__ . "/{$bot_username}_update.log");
-        
-        // Enable MySQL
-        $pdo = get_db();
-        $telegram->enableExternalMySql($pdo);
 
+        
+
+        
         // Handle telegram webhook request
-        if ($is_hook)
-        {
+        if ($is_hook) {
             $server_response = $telegram->handle();
-        }
-        else
-        {
+        } else {
             $server_response = $telegram->handleGetUpdates();
             
             if ($server_response->isOk()) {
@@ -84,7 +115,6 @@ function bot($is_hook)
                 echo date('Y-m-d H:i:s', time()) . ' - Failed to fetch updates' . PHP_EOL;
                 echo $server_response->printError();
             }
-            
         }
         /*
          * $results = Longman\TelegramBot\Request::sendToActiveChats(
