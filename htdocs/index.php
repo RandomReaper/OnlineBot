7533eaf18fabe84b76a6768eec531f32f57fe8d6
Marc Pignat <marc@pignat.org>
2017-10-04 16:15:27 +0200

    remove shebang on index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,10 @@
-#!/usr/bin/env php
 <?php
 
 error_reporting(E_ALL);
 
 require_once(__DIR__ . '/../src/bot.php');
 
-if (!isset($_SERVER["HTTP_HOST"])) {
+if (!isset($_SERVER["HTTP_HOST"]) && $argc > 1) {
     parse_str($argv[1], $_GET);
     parse_str($argv[1], $_POST);
 }
@@ -20,8 +19,7 @@ if(isset($_POST['uid']))
     
     // looks like an online server
     $ret = online($_POST['uid'], $name);
-}
-    
+} 
 
 $pdo = get_db();
 foreach($pdo->query('SELECT * FROM ob_servers') as $row) {
---
+++ index.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php htdocs/index.php "$@"
