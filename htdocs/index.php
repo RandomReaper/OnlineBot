3734e53d40aafd2484d56797cd8f99849f0b7ba1
Marc Pignat <marc@pignat.org>
2021-10-04 11:10:12 +0200

    fix html

--- htdocs/index.php
+++ htdocs/index.php
@@ -67,8 +67,10 @@ if ($online && !isset($_REQUEST['uid']))
 {
 $html =<<<EOT
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+<meta charset="utf-8" />
+<title>a.pignat.org</title>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <meta name="google-site-verification" content="JzxEzqkYg7kk74ZMjq4v-4NLSY6GboR2MyJNl8O4xOs" />
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119405595-1"></script>
@@ -92,7 +94,7 @@ font-family:sans
 <p>Receive a telegram when your host is offline.</p>
 <h2>Telegram side</h2>
 Talk to the <a href="https://telegram.me/PimOnlineBot">bot</a>, then ask for <b>/help</b>.
-<h2>Host side<h2>
+<h2>Host side</h2>
 The current help is only on the bot side.
 <h2>License and sources</h2>
 © 2017 Marc Pignat, licensed under the <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL v3</a>, sources on <a href="https://github.com/RandomReaper/OnlineBot">github</a>.
