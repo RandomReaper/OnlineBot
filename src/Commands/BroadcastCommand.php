348a289d559fb7cb23319a75f88ea583c67f0c7c
Adrien Reynard <reynard.adrien.08@gmail.com>
2026-01-23 08:45:39 +0100

    Add broadcast message

---
+++ src/Commands/BroadcastCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\DB;
+
+class BroadcastCommand extends UserCommand
+{
+    protected $name = 'broadcast';
+    protected $description = 'Broadcast message to all users';
+    protected $usage = '/broadcast <message>';
+    protected $version = '1.0.0';
+
+    public function execute(): ServerResponse
+    {
+        require __DIR__ . '/../../config/config.php';
+
+        $message = $this->getMessage();
+        $chat_id = $message->getChat()->getId();
+        $user_id = $message->getFrom()->getId();
+        $text = trim($message->getText(true));
+
+        if (!isset($admins_id) || !in_array($user_id, $admins_id)) {
+            return Request::sendMessage([
+                'chat_id'    => $chat_id,
+                'text'       => '*Error:* You do not have permission to execute this command.',
+                'parse_mode' => 'Markdown'
+            ]);
+        }
+
+        if ($text === '') {
+            return Request::sendMessage([
+                'chat_id'    => $chat_id,
+                'text'       => '*Error*: Message body is empty. Usage: `/broadcast <message>`',
+                'parse_mode' => 'Markdown'
+            ]);
+        }
+
+        $results = DB::getPdo()->query('SELECT id FROM user')->fetchAll(\PDO::FETCH_COLUMN);
+
+        foreach ($results as $target_id) {
+            Request::sendMessage([
+                'chat_id' => $target_id,
+                'text'    => "*Important message*\n\n" . $text,
+                'parse_mode' => 'Markdown'
+            ]);
+        }
+
+        return Request::sendMessage([
+            'chat_id' => $chat_id,
+            'text'    => 'Broadcast sent to ' . count($results) . ' users.'
+        ]);
+    }
+}
